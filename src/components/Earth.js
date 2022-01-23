08c8dba131877bc49e20fddca0496627c8f7440c
Kris <55980434+Kristiqn95@users.noreply.github.com>
2022-01-23 10:29:42 +0200

    fix: changed to pivot

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -1,19 +1,21 @@
-import { Container, Sprite } from 'pixi.js-legacy';
+import { Container, Sprite } from "pixi.js-legacy";
 
-import gsap from 'gsap';
+import gsap from "gsap";
 
 import Rocket from "./Rocket";
 
 export default class Earth extends Container {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.name = "earth";
-        this._body = Sprite.from('earth');
-        this._body.anchor.set(0.5);
-        this.addChild(this._body);
+    this.name = "earth";
+    this._body = Sprite.from('earth');
+    this._body.anchor.set(0.5);
+    this.addChild(this._body);
 
-        this._rocket = new Rocket();
-        this.addChild(this._rocket);
-    }
+    this._rocket = new Rocket();
+    this._rocket.pivot.set(-350,0);
+    this.addChild(this._rocket);
+
+  }
 }
